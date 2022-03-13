6b1156a9719ac373fbc18bf88b298a51d2f3d85a
Joel Fintan <joel@sapaad.com>
2022-03-13 22:15:02 +0530

    Added notifications permission

--- src/containers/userPermissionsWatcher/index.tsx
+++ src/containers/userPermissionsWatcher/index.tsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react";
 import { IUserPermissionWatcherProps } from "./types";
 
 const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
-  return <>{props.children}</>;
+  const [notificationSupport, setNotificationSupport] = useState(false);
+  const [notificationStatus, setNotificationStatus] = useState("default");
+
+  useEffect(() => {
+    setNotificationSupport("Notification" in window);
+  }, []);
+
+  useEffect(() => {
+    if (notificationSupport) {
+      Notification.requestPermission()
+        .then((value) => {
+          setNotificationStatus(value);
+          console.log(value);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [notificationSupport]);
+
+  return notificationStatus === "granted" && notificationSupport ? (
+    <>{props.children}</>
+  ) : (
+    <div>
+      <p>Is notification Supported? {notificationSupport ? "YES" : "NO"}</p>
+      <p>
+        Is notification granted?{" "}
+        {notificationStatus === "granted"
+          ? "YES"
+          : notificationStatus === "denied"
+          ? "Nope. pls check site settings."
+          : "You skipped the request! :< Pls check site settings"}
+      </p>
+    </div>
+  );
 };
 export default UserPermissionWatcher;
