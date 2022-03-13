f2f8c7b04fe03150cf3f1592d33a593ff7d85959
Joel Fintan <joel@sapaad.com>
2022-03-13 23:57:02 +0530

    Added some styling to the app

--- src/containers/userPermissionsWatcher/index.tsx
+++ src/containers/userPermissionsWatcher/index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import { Card, Icon, Message } from "semantic-ui-react";
 import { IUserPermissionWatcherProps } from "./types";
 
 const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
   const [notificationSupport, setNotificationSupport] = useState(false);
   const [notificationStatus, setNotificationStatus] = useState("default");
+  const [notificationLoading, setNotificationLoading] = useState(false);
 
   useEffect(() => {
     setNotificationSupport("Notification" in window);
@@ -11,30 +13,53 @@ const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
 
   useEffect(() => {
     if (notificationSupport) {
+      setNotificationLoading(true);
       Notification.requestPermission()
         .then((value) => {
+          setNotificationLoading(false);
           setNotificationStatus(value);
-          console.log(value);
         })
         .catch((error) => {
-          console.log(error);
+          setNotificationLoading(false);
         });
     }
   }, [notificationSupport]);
 
-  return notificationStatus === "granted" && notificationSupport ? (
+  return notificationStatus !== "granted" && !notificationSupport ? (
     <>{props.children}</>
   ) : (
-    <div>
-      <p>Is notification Supported? {notificationSupport ? "YES" : "NO"}</p>
-      <p>
-        Is notification granted?{" "}
-        {notificationStatus === "granted"
-          ? "YES"
-          : notificationStatus === "denied"
-          ? "Nope. pls check site settings."
-          : "You skipped the request! :< Pls check site settings"}
-      </p>
+    <div className="user-permission-container">
+      <h1>User Permissions</h1>
+
+      <div className="permissions-wrapper">
+        <Message
+          positive={notificationSupport && notificationStatus === "granted"}
+          warning={notificationSupport && notificationStatus !== "granted"}
+          negative={!notificationSupport}
+          icon
+        >
+          <Icon
+            name={notificationLoading ? "circle notched" : "mobile alternate"}
+            loading={notificationLoading}
+          />
+          <Message.Content>
+            <Message.Header>{"Browser Notifications"}</Message.Header>
+            {!notificationSupport
+              ? "This browser does not support push notifications"
+              : notificationStatus === "granted"
+              ? "Notifications are enabled."
+              : notificationStatus === "denied"
+              ? "Notification permission is blocked by user. Please enable them from browser settings"
+              : "Waiting for user confirmation. If skipped, check site settings in browser settings"}
+          </Message.Content>
+        </Message>
+        <Message
+          negative
+          icon={"location arrow"}
+          header="Location"
+          content="Location disabled by user"
+        />
+      </div>
     </div>
   );
 };
