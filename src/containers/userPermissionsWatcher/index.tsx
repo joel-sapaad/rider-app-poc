0a8e000d7200546ee5beb653950d1bc807f9f02f
Shameer Sapaad <shameer@sapaad.com>
2022-03-14 12:45:21 +0530

    Prompt for location access

--- src/containers/userPermissionsWatcher/index.tsx
+++ src/containers/userPermissionsWatcher/index.tsx
@@ -1,21 +1,47 @@
 import { useEffect, useState } from "react";
-import { Card, Icon, Message } from "semantic-ui-react";
+import { Button, Card, Icon, Message } from "semantic-ui-react";
 import { IUserPermissionWatcherProps } from "./types";
 
 const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
   const [notificationSupport, setNotificationSupport] = useState(false);
   const [notificationStatus, setNotificationStatus] = useState("default");
   const [notificationLoading, setNotificationLoading] = useState(false);
+  const [locationSupport, setLocationSupport] = useState(false);
+  const [locationStatus, setLocationStatus] = useState("default");
 
   useEffect(() => {
     setNotificationSupport("Notification" in window);
+    setLocationSupport("geolocation" in navigator);
   }, []);
+
+  useEffect(() => {
+    if (locationSupport) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            console.log(result.state);
+            setLocationStatus(result.state);
+            //If granted then you can directly call your function here
+          } else if (result.state === "prompt") {
+            navigator.permissions;
+            navigator.geolocation.getCurrentPosition((location) => {});
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+          result.onchange = function () {
+            setLocationStatus(result.state);
+          };
+        });
+    }
+  }, [locationSupport]);
 
   useEffect(() => {
     if (notificationSupport) {
       setNotificationLoading(true);
       Notification.requestPermission()
         .then((value) => {
+          console.log(value);
           setNotificationLoading(false);
           setNotificationStatus(value);
         })
@@ -25,7 +51,9 @@ const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
     }
   }, [notificationSupport]);
 
-  return notificationStatus === "granted" && notificationSupport ? (
+  return notificationSupport &&
+    notificationStatus === "granted" &&
+    locationStatus === "granted" ? (
     <>{props.children}</>
   ) : (
     <div className="user-permission-container">
@@ -55,6 +83,7 @@ const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
         </Message>
         <Message
           negative
+          positive={locationSupport && locationStatus === "granted"}
           icon={"location arrow"}
           header="Location"
           content="Location disabled by user"
