f90584b7dc73fb6ad8e77ba4478178865ed0ba2f
Joel Fintan <joel@sapaad.com>
2022-03-14 13:48:41 +0530

    Added Location permission

--- src/containers/userPermissionsWatcher/index.tsx
+++ src/containers/userPermissionsWatcher/index.tsx
@@ -1,31 +1,77 @@
 import { useEffect, useState } from "react";
-import { Card, Icon, Message } from "semantic-ui-react";
+import { Button, Icon, Message } from "semantic-ui-react";
 import { IUserPermissionWatcherProps } from "./types";
 
 const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
   const [notificationSupport, setNotificationSupport] = useState(false);
   const [notificationStatus, setNotificationStatus] = useState("default");
   const [notificationLoading, setNotificationLoading] = useState(false);
+  const [locationSupport, setLocationSupport] = useState(false);
+  const [locationStatus, setLocationStatus] = useState("default");
+  const [locationLoading, setLocationLoading] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lon: 0 });
 
   useEffect(() => {
     setNotificationSupport("Notification" in window);
+    setLocationSupport("geolocation" in navigator);
   }, []);
 
   useEffect(() => {
-    if (notificationSupport) {
-      setNotificationLoading(true);
-      Notification.requestPermission()
-        .then((value) => {
-          setNotificationLoading(false);
-          setNotificationStatus(value);
-        })
-        .catch((error) => {
-          setNotificationLoading(false);
+    if (locationSupport) {
+      setLocationLoading(true);
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            console.log(result.state);
+            setLocationStatus(result.state);
+            setLocationLoading(false);
+            //If granted then you can directly call your function here
+          } else if (result.state === "prompt") {
+            setLocationLoading(true);
+            navigator.permissions;
+            navigator.geolocation.getCurrentPosition((location) => {
+              setLocationLoading(false);
+            });
+          } else if (result.state === "denied") {
+            setLocationLoading(false);
+            //If denied then you have to show instructions to enable location
+          }
+          if (notificationSupport) {
+            requestNotificationPermission();
+          }
+          result.onchange = function () {
+            setLocationLoading(false);
+            setLocationStatus(result.state);
+          };
         });
     }
-  }, [notificationSupport]);
+  }, [locationSupport]);
 
-  return notificationStatus === "granted" && notificationSupport ? (
+  const requestLocation = () => {
+    navigator.permissions;
+    navigator.geolocation.getCurrentPosition((location) => {
+      const { latitude, longitude } = location.coords;
+      setCurrentLocation({ lat: latitude, lon: longitude });
+    });
+  };
+
+  const requestNotificationPermission = () => {
+    setNotificationLoading(true);
+    Notification.requestPermission()
+      .then((value) => {
+        console.log(value);
+        setNotificationLoading(false);
+        setNotificationStatus(value);
+      })
+      .catch((error) => {
+        setNotificationLoading(false);
+      });
+  };
+
+  return notificationSupport &&
+    notificationStatus === "granted" &&
+    locationStatus === "granted" ? (
     <>{props.children}</>
   ) : (
     <div className="user-permission-container">
@@ -54,11 +100,37 @@ const UserPermissionWatcher = (props: IUserPermissionWatcherProps) => {
           </Message.Content>
         </Message>
         <Message
-          negative
-          icon={"location arrow"}
-          header="Location"
-          content="Location disabled by user"
-        />
+          positive={locationSupport && locationStatus === "granted"}
+          warning={locationSupport && locationStatus !== "granted"}
+          negative={!locationSupport}
+          icon
+        >
+          <Icon
+            name={locationLoading ? "circle notch" : "location arrow"}
+            loading={locationLoading}
+          />
+          <Message.Content>
+            <Message.Header>{"Geolocation"}</Message.Header>
+            {!locationSupport
+              ? "This browser does not support getting user's location"
+              : locationStatus === "granted"
+              ? "Location permission has been allowed by user"
+              : locationStatus === "granted"
+              ? "Location permission denied by user"
+              : "Waiting for user confirmation. If skipped/denied, check site settings in browser settings"}
+            <br />
+            {locationStatus === "granted" && (
+              <>
+                <Button size="small" icon onClick={requestLocation}>
+                  <Icon name="location arrow" /> Request Location
+                </Button>{" "}
+                {currentLocation.lat > 0
+                  ? `${currentLocation.lat},${currentLocation.lon}`
+                  : " "}
+              </>
+            )}
+          </Message.Content>
+        </Message>
       </div>
     </div>
   );
