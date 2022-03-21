c36efbefc5a8dd6198242de0f5af103a2eedad17
Joel Fintan <joel@sapaad.com>
2022-03-21 23:14:16 +0530

    added onclick event for notification item

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
 self.addEventListener("push", (event) => {
   const data = event.data.json();
 
-  self.registration.showNotification(data.title, { body: data.body });
+  self.registration.showNotification(data.title, { body: data.body, tag: "1" });
 });
+
+self.onnotificationclick = function (event) {
+  console.log("On notification click: ", event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then(function (clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == "/" && "focus" in client) return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow("/");
+      })
+  );
+};
