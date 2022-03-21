4075865e7974205bcbc2e3c147cdf0650e3baa6f
Joel Fintan <joel@sapaad.com>
2022-03-21 23:24:48 +0530

    added require interaction

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
 self.addEventListener("push", (event) => {
   const data = event.data.json();
 
-  self.registration.showNotification(data.title, { body: data.body, tag: "1" });
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    tag: "1",
+    requireInteraction: true,
+  });
 });
 
 self.onnotificationclick = function (event) {
