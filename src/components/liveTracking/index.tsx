8ab31392be4a57eec3557586056d2ce813438e14
Shameer Sapaad <shameer@sapaad.com>
2022-03-28 12:13:03 +0530

    Watch position when location is updated

--- src/components/liveTracking/index.tsx
+++ src/components/liveTracking/index.tsx
@@ -5,7 +5,36 @@ import { RootState } from "../../redux/store";
 const LiveTracking = () => {
   const user = useSelector((state: RootState) => state.user);
   useEffect(() => {
+    var id: any, target: any, options: any;
 
+    function success(pos: any) {
+      var crd = pos.coords;
+      console.log(crd);
+      if (
+        target.latitude === crd.latitude &&
+        target.longitude === crd.longitude
+      ) {
+        console.log("Congratulations, you reached the target");
+        navigator.geolocation.clearWatch(id);
+      }
+    }
+
+    function error(err: any) {
+      console.warn("ERROR(" + err.code + "): " + err.message);
+    }
+
+    target = {
+      latitude: 52.520007,
+      longitude: 13.404954,
+    };
+
+    options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    id = navigator.geolocation.watchPosition(success, error, options);
   }, []);
   return (
     <div>
