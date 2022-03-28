a7447fc83582bbcd45ad2dc143541ba2de3e2804
Joel Fintan <joel@sapaad.com>
2022-03-28 12:58:46 +0530

    Added location tracking feature

--- src/components/liveTracking/index.tsx
+++ src/components/liveTracking/index.tsx
@@ -1,16 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
 const LiveTracking = () => {
   const user = useSelector((state: RootState) => state.user);
-  useEffect(() => {
+  const [coords, setCoords] = useState({ lat: 0, lon: 0 });
 
+  useEffect(() => {
+    let id: any, target: any, options: any;
+    
+    function success(pos:GeolocationPosition) {
+      let {latitude,longitude} = pos.coords
+      setCoords({ lat: latitude, lon: longitude });
+      let crd = pos.coords;
+      console.log(crd);
+      if (
+        target.latitude === crd.latitude &&
+        target.longitude === crd.longitude
+      ) {
+        console.log("Congratulations, you reached the target");
+        navigator.geolocation.clearWatch(id);
+      }
+    }
+
+    function error(err: any) {
+      console.warn("ERROR(" + err.code + "): " + err.message);
+    }
+
+    target = {
+      latitude: 52.520007,
+      longitude: 13.404954,
+    };
+
+    options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    id = navigator.geolocation.watchPosition(success, error, options);
   }, []);
   return (
     <div>
       <h1>Welcome {user.email}</h1>
       <h2>Live tracking test</h2>
+      <h3>Latitude: {coords.lat}</h3>
+      <h3>Longitude: {coords.lon}</h3>
     </div>
   );
 };
